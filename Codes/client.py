bc80f8ff8bd53c2a5dedcf70969fbb0311d67734
shishir-me <shishir.baidya.me@gmail.com>
2025-05-10 23:59:15 +0600

    writing for client and  server code

--- Codes/client.py
+++ Codes/client.py
@@ -1,1 +1,26 @@
-import socket,threading
+import socket
+import threading
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 24131))
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode()
+            print(message)
+        except:
+            print("An error occurred!")
+            client.close()
+            break
+
+def write():
+    while True:
+        message = input('')
+        client.send(message.encode())
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
--- Codes/server.py
+++ Codes/server.py
@@ -1,1 +1,40 @@
-import socket
+import socket,threading
+
+clients=[]
+
+def handle_client(client_socket):
+    while True:
+        try:
+            data=client_socket.recv(1024)
+            if not data:
+                break
+            broadcast(data,client_socket)
+        except:
+            break
+    
+    clients.remove(client_socket)
+    client_socket.close()
+
+def broadcast(message,sender_socket):
+    for client in clients :
+        if client !=sender_socket:
+            try:
+                client.send(message)
+            except:
+                client.close()
+                client.remove(client)
+def start_server():
+    server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    server.bind(('127.0.0.1',24131))
+    server.listen()
+    print('Server is started listening:')
+
+    while True:
+        clients_socket,addr=server.accept()
+        print('New Connection is happening: ',clients_socket,addr)
+        clients.append(clients_socket)
+        thread = threading.Thread(target=handle_client,args=(clients_socket,))
+        thread.start()
+
+if __name__=='__main__':
+    start_server()
